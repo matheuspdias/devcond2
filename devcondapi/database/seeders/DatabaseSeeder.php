c2efc6f2090d4d5e3998661e22a1faf335dc57d8
Matheus Pereira <matheus.tecinfor98@gmail.com>
2021-01-12 13:22:34 -0300

    API - criando seeder para popular banco de dados

--- devcondapi/database/seeders/DatabaseSeeder.php
+++ devcondapi/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,63 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        /* INSERINDO UNIDADES   */
+
+        DB::table('units')->insert([
+            'name' => 'APT 100',
+            'id_owner' => 1
+        ]);
+        DB::table('units')->insert([
+            'name' => 'APT 101',
+            'id_owner' => 1
+        ]);
+        DB::table('units')->insert([
+            'name' => 'APT 200',
+            'id_owner' => '0'
+        ]);
+        DB::table('units')->insert([
+            'name' => 'APT 201',
+            'id_owner' => '0'
+        ]);
+
+        /*  INSERINDO AREAS */
+
+        DB::table('areas')->insert([
+            'allowed' => '1',
+            'title' => 'Academia',
+            'cover' => 'gym.jpg',
+            'days' => '1,2,4,5',
+            'start_time' => '06:00:00',
+            'end_time' => '22:00:00'
+        ]);
+        DB::table('areas')->insert([
+            'allowed' => '1',
+            'title' => 'Piscina',
+            'cover' => 'pool.jpg',
+            'days' => '1,2,3,4,5',
+            'start_time' => '07:00:00',
+            'end_time' => '23:00:00'
+        ]);
+        DB::table('areas')->insert([
+            'allowed' => '1',
+            'title' => 'Churrasqueira',
+            'cover' => 'barbecue.jpg',
+            'days' => '4,5,6',
+            'start_time' => '09:00:00',
+            'end_time' => '22:00:00'
+        ]);
+
+        /*  INSERINDO AVISOS   */
+
+        DB::table('walls')->insert([
+            'title' => 'Título de Aviso de Teste',
+            'body' => 'Lorem ipsum blablalba  lorem ipsim',
+            'datecreated' => '2020-12-20 15:00:00'
+        ]);
+        DB::table('walls')->insert([
+            'title' => 'Alerta geral para todos',
+            'body' => 'Cuidado com blablalba  lorem ipsim',
+            'datecreated' => '2020-12-20 18:00:00'
+        ]);
     }
 }
