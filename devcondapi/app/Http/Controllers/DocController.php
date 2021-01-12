e34056c4d0184a4d7f8516092be0abee6c0b4a18
Matheus Pereira <matheus.tecinfor98@gmail.com>
2021-01-12 14:58:56 -0300

    API - lista de docs geral

--- devcondapi/app/Http/Controllers/DocController.php
+++ devcondapi/app/Http/Controllers/DocController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Doc;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class DocController extends Controller
 {
-    //
+    public function getAll() {
+        $array = ['error' => ''];
+
+        $docs = Doc::all();
+
+        foreach($docs as $docKey => $docValue) {
+            $docs[$docKey]['fileurl'] = asset('storage/'.$docValue['fileurl']);
+        }
+
+        $array['list'] = $docs;
+
+        return $array;
+    }
 }
