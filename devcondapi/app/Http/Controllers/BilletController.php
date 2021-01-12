ce27c23ce9d56a251feba76f6afdad7a982699d3
Matheus Pereira <matheus.tecinfor98@gmail.com>
2021-01-12 17:35:49 -0300

    API - listando boletos de unidades

--- devcondapi/app/Http/Controllers/BilletController.php
+++ devcondapi/app/Http/Controllers/BilletController.php
@@ -2,9 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Billet;
+use App\Models\Unit;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class BilletController extends Controller
 {
-    //
+    public function getAll(Request $request) {
+        $array = ['error' => ''];
+
+        $property = $request->input('property');
+        if($property) {
+            $user = Auth()->user();
+            
+            $unit = Unit::where('id', $property)
+            ->where('id_owner', $user['id'])
+            ->count();
+
+            if($unit > 0) {
+            $billets = Billet::where('id_unit', $property)->get();
+
+            foreach($billets as $billetKey => $billetValue) {
+                $billets[$billetKey]['fileurl'] = asset('/storage'.$billetValue['fileurl']);
+
+            }
+
+            $array['list'] = $billets;
+
+            } else {
+                $array['error'] = 'Está unidade não é sua';
+            }     
+
+        } else {
+            $array['error'] = 'A propriedade é necessária';
+        }
+
+        return $array;
+    }
 }
