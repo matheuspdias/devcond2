bb14a49f6d442956bdb2c828d75660ed2001cb39
Matheus Pereira <matheus.tecinfor98@gmail.com>
2021-01-12 12:55:41 -0300

    API - Login de usuario

--- devcondapi/app/Http/Controllers/AuthController.php
+++ devcondapi/app/Http/Controllers/AuthController.php
@@ -69,4 +69,44 @@ class AuthController extends Controller
 
         return $array;
     }
+
+    public function login (Request $request) {
+        $array = ['error' => ''];
+
+        $validator = Validator::make($request->all(), [
+            'cpf' => 'required|digits:11',
+            'password' => 'required'
+        ]);
+
+        if(!$validator->fails()) {
+            $cpf = $request->input('cpf');
+            $password = $request->input('password');
+
+            $token = auth()->attempt([
+                'cpf' => $cpf,
+                'password' => $password
+            ]);
+
+            if(!$token) {
+                $array['error'] = 'CPF e/ou senha incorretos!';
+                return $array;
+            }
+
+            $array['token'] = $token;
+
+            $user = auth()->user();
+            $array['user'] = $user;
+
+            $properties = Unit::select(['id', 'name'])
+            ->where('id_owner', $user['id'])
+            ->get();
+
+            $array['user']['properties'] = $properties;
+
+        } else {
+            $array['error'] = $validator->errors()->first();
+        }
+
+        return $array;
+    }
 }
