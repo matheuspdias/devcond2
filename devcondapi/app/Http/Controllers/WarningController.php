3e2e36758cf19ef646303315f4884e0b2e0eecda
Matheus Pereira <matheus.tecinfor98@gmail.com>
2021-01-12 18:31:21 -0300

    API - listando meus avisos

--- devcondapi/app/Http/Controllers/WarningController.php
+++ devcondapi/app/Http/Controllers/WarningController.php
@@ -2,9 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Unit;
+use App\Models\Warning;
 use Illuminate\Http\Request;
 
 class WarningController extends Controller
 {
-    //
+    public function getMyWarnings(Request $request) {
+        $array = ['error' => ''];
+
+        $property = $request->input('property');
+        if($property) {
+            $user = Auth()->user();
+
+            $unit = Unit::where('id', $property)
+            ->where('id_owner', $user['id'])
+            ->count();
+
+            if($unit > 0) {
+
+                $warnings = Warning::where('id_unit', $property)
+                ->orderBy('datecreated', 'DESC')
+                ->orderBy('id', 'DESC')
+                ->get();
+
+                foreach($warnings as $warnKey => $warnValue) {
+                    $warnings[$warnKey]['datecreated'] = date('d/m/Y', strtotime($warnValue['datecreated']));
+                    $photoList = [];
+                    $photos = explode(',', $warnValue['photos']);
+                    foreach($photos as $photo) {
+                        if(!empty($photo)) {
+                            $photoList[] = asset('storage/'.$photo);
+                        }
+                    }
+
+                    $warnings[$warnKey]['photos'] = $photoList;
+                }
+
+                $array['list'] = $warnings;
+
+            } else {
+                $array['error'] = 'Esta unidade não é sua!';
+            }
+
+        } else {
+            $array['error'] = 'A propriedade é necessária!';
+        }
+
+        return $array;
+    }
 }
