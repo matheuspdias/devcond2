8bfcc2e9853e02ba20fd0cbc548fc175a39800b9
Matheus Pereira <matheus.tecinfor98@gmail.com>
2021-01-12 13:48:13 -0300

    API - /walls listando avisos do mural

--- devcondapi/app/Http/Controllers/WallController.php
+++ devcondapi/app/Http/Controllers/WallController.php
@@ -3,8 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use App\Models\Wall;
+use App\Models\WallLike;
 
 class WallController extends Controller
 {
-    //
+    public function getAll() {
+        $array = ['error' => '', 'list' => []];
+
+        $user = Auth()->user();
+
+        $walls = Wall::all();
+
+        foreach($walls as $wallKey => $wallValue) {
+            $walls[$wallKey]['likes'] = 0;
+            $walls[$wallKey]['liked'] = false;
+
+            $likes = WallLike::where('id_wall', $wallValue['id'])->count();
+            $walls[$wallKey]['likes'] = $likes;
+
+            $meLikes = WallLike::where('id_wall', $wallValue['id'])->where('id_user', $user['id'])
+            ->count();
+
+            if($meLikes > 0) {
+                $walls[$wallKey]['liked'] = true;
+            }
+        }
+
+        $array['list'] = $walls;
+
+        return $array;
+    }
 }
