d40520d070854a3fa3ca7c4b1e0ab3c0aa1e2b73
Matheus Pereira <matheus.tecinfor98@gmail.com>
2021-01-12 12:36:11 -0300

    API - registro e autenticação de usuário

--- devcondapi/app/Http/Controllers/AuthController.php
+++ devcondapi/app/Http/Controllers/AuthController.php
@@ -2,9 +2,71 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Unit;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
 
 class AuthController extends Controller
 {
-    //
+    public function unauthorized() {
+        return response()->json([
+            'error' => 'Não autorizado'
+        ], 401);
+    }
+
+    public function register(Request $request) {
+        $array = ['error' => ''];
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'cpf' => 'required|digits:11|unique:users,cpf',
+            'password' => 'required',
+            'password_confirm' => 'required|same:password'
+        ]);
+
+        if(!$validator->fails()) {
+            $name = $request->input('name');
+            $email = $request->input('email');
+            $cpf = $request->input('cpf');
+            $password = $request->input('password');
+
+            $hash = password_hash($password, PASSWORD_DEFAULT);
+
+            $newUser = new User();
+            $newUser->name = $name;
+            $newUser->email = $email;
+            $newUser->cpf = $cpf;
+            $newUser->password = $hash;
+            $newUser->save();
+
+            $token = auth()->attempt([
+                'cpf' => $cpf,
+                'password' => $password
+            ]);
+
+            if(!$token) {
+                $array['error'] = 'Ocorreu um erro';
+                return $array;
+            }
+
+            $array['token'] = $token;
+
+            $user = auth()->user();
+            $array['user'] = $user;
+
+            $properties = Unit::select(['id', 'name'])
+            ->where('id_owner', $user['id'])
+            ->get();
+
+            $array['user']['properties'] = $properties;
+
+        } else {
+            $array['error'] = $validator->errors()->first();
+            return $array;
+        }
+
+        return $array;
+    }
 }
