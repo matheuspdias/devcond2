59fb1994aee30d14096e1f88f964d1c8bb77196a
Matheus Pereira <matheus.tecinfor98@gmail.com>
2021-01-13 12:05:07 -0300

    API - lista achados e perdidos

--- devcondapi/app/Http/Controllers/FoundAndLostController.php
+++ devcondapi/app/Http/Controllers/FoundAndLostController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\FoundAndLost;
 use Illuminate\Http\Request;
 
 class FoundAndLostController extends Controller
 {
-    //
+    public function getAll() {
+        $array = ['error' => ''];
+
+        $lost = FoundAndLost::where('status', 'LOST')
+        ->orderBy('datecreated', 'DESC')
+        ->orderBy('id', 'DESC')
+        ->get();
+
+        $recovered = FoundAndLost::where('status', 'RECOVERED')
+        ->orderBy('datecreated', 'DESC')
+        ->orderBy('id', 'DESC')
+        ->get();
+
+        foreach($lost as $lostkey => $lostValue) {
+            $lost[$lostkey]['datecreated'] = date('d/m/Y', strtotime($lostValue['datecreated']));
+            $lost[$lostkey]['photo'] = asset('storage/'.$lostValue['photo']);
+        }
+        foreach($recovered as $recKey => $recValue) {
+            $recovered[$recKey]['datecreated'] = date('d/m/Y', strtotime($recValue['datecreated']));
+            $recovered[$recKey]['photo'] = asset('storage/'.$recValue['photo']);
+        }
+
+        $array['lost'] = $lost;
+        $array['recovered'] = $recovered;
+
+        return $array;
+    }
 }
