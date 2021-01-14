efb47c0553eb25a4eeea1eaa9251b523ccdfe124
Matheus Pereira <matheus.tecinfor98@gmail.com>
2021-01-14 14:45:13 -0300

    API - Listando horarios disponiveis para o dia

--- devcondapi/app/Http/Controllers/ReservationController.php
+++ devcondapi/app/Http/Controllers/ReservationController.php
@@ -117,6 +117,90 @@ class ReservationController extends Controller
         return $array;
     }
 
+    public function getTimes($id, Request $request) {
+        $array = ['error' => '', 'list' => []];
+
+        $validator = Validator::make($request->all(), [
+            'date' => 'required|date_format:Y-m-d'
+        ]);
+        if(!$validator->fails()) {
+            $date = $request->input('date');
+            $area = Area::find($id);
+
+            if($area) {
+
+                $can = true;
+
+                // Verificar se é dia disabled
+                $existingDisabledDay = AreaDisabledDay::where('id_area', $id)
+                ->where('day', $date)
+                ->count();
+                if($existingDisabledDay > 0) {
+                    $can = false;
+                }
+
+                // Verificar se é dia permitido
+                $allowedDays = explode(',', $area['days']);
+                $weekday = date('w', strtotime($date));
+                if(!in_array($weekday, $allowedDays)) {
+                    $can = false;
+                }
+
+                if($can) {
+                    $start = strtotime($area['start_time']);
+                    $end = strtotime($area['end_time']);
+                    $times = [];
+
+                    for(
+                        $lastTime = $start;
+                        $lastTime < $end;
+                        $lastTime = strtotime('+1 hour', $lastTime)
+                    ) {
+                        $times[] = $lastTime;
+                    }
+
+                    $timeList = [];
+                    foreach($times as $time) {
+                        $timeList[] = [
+                            'id' => date('H:i:s', $time),
+                            'title' => date('H:i', $time).' - '.date('H:i', strtotime('+1 hour', $time))
+                        ];
+                    } 
+
+                    // Removendo horarios reservados
+                    $reservations = Reservation::where('id_area', $id)
+                    ->whereBetween('reservation_date', [
+                        $date.' 00:00:00',
+                        $date.' 23:59:59'
+                    ])
+                    ->get();
+
+                    $toRemove = [];
+                    foreach($reservations as $reservation) {
+                        $time = date('H:i:s', strtotime($reservation['reservation_date']));
+                        $toRemove[] = $time;
+                    }
+
+                    foreach($timeList as $timeItem) {
+                        if(!in_array($timeItem['id'], $toRemove)) {
+                            $array['list'][] = $timeItem;
+                        }
+                    }
+                                        
+                }
+
+            } else {
+                $array['error'] = 'Area inexistente!';
+                return $array;
+            }
+
+        } else {
+            $array['error'] = $validator->errors()->first();
+        }
+
+        return $array;
+    }
+
     public function setReservation($id, Request $request) {
         $array = ['error' => ''];
 
