6f0120d4efb6a164c000441104e1e78d78276559
Matheus Pereira <matheus.tecinfor98@gmail.com>
2021-01-14 22:50:09 -0300

    API - Deletando uma reserva

--- devcondapi/app/Http/Controllers/ReservationController.php
+++ devcondapi/app/Http/Controllers/ReservationController.php
@@ -310,4 +310,29 @@ class ReservationController extends Controller
 
         return $array;
     }
+
+    public function delMyReservation($id) {
+        $array = ['error' => ''];
+
+        $user = auth()->user();
+        $reservation = Reservation::find($id);
+        if($reservation) {
+            $unit = Unit::where('id', $reservation['id_unit'])
+            ->where('id_owner', $user['id'])
+            ->count();
+
+            if($unit > 0) {
+                Reservation::find($id)->delete();
+            } else {
+                $array['error'] = 'Está reserva não é sua';
+                return $array;
+            }
+
+        } else {
+            $array['error'] = 'Reserva inexistente!';
+            return $array;
+        }
+
+        return $array;
+    }
 }
